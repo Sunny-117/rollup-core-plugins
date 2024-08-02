28eecf4bac61d60c51d37a66d6c54a4f80840009
Sunny-117 <zhiqiangfu6@gmail.com>
2024-08-02 15:56:12 +0800

    feat: determine whether to join terser plugin based on env

--- tutorial/quickstart/rollup.config.js
+++ tutorial/quickstart/rollup.config.js
@@ -3,27 +3,32 @@ import { defineConfig } from 'rollup'
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
-export default defineConfig({
-  // input: ['src/main.js'],
-  input: 'src/main.ts',
-  output: {
-    // dir: 'dist',
-    file: 'dist/bundle.cjs.js',//输出文件的路径和名称
-    format: 'iife',//五种输出格式：amd/es6/iife/umd/cjs
-    name: 'bundleName',//当format为iife和umd时必须提供，将作为全局变量挂在window下
-    globals: {
-      lodash: '_', //告诉rollup全局变量_即是lodash
-      jquery: '$' //告诉rollup全局变量$即是jquery
-    }
-  },
-  external: ['lodash', 'jquery'],
-  plugins: [
-    babel({
-      exclude: "node_modules/**"
-    }),
-    resolve(),
-    commonjs(),
-    typescript()
-  ]
-})
+export default ({ prod }) => {
+  return defineConfig({
+    // input: ['src/main.js'],
+    input: 'src/main.ts',
+    output: {
+      // dir: 'dist',
+      file: 'dist/bundle.cjs.js',//输出文件的路径和名称
+      format: 'iife',//五种输出格式：amd/es6/iife/umd/cjs
+      name: 'bundleName',//当format为iife和umd时必须提供，将作为全局变量挂在window下
+      globals: {
+        lodash: '_', //告诉rollup全局变量_即是lodash
+        jquery: '$' //告诉rollup全局变量$即是jquery
+      }
+    },
+    external: ['lodash', 'jquery'],
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      resolve(),
+      commonjs(),
+      typescript(),
+      ...(prod ? [terser()] : [])
+    ]
+  })
+
+}

bd6671cebbb319d127b8b4e369349099bd5363f8
Sunny-117 <zhiqiangfu6@gmail.com>
2024-08-02 15:44:06 +0800

    feat: cdn

--- tutorial/quickstart/rollup.config.js
+++ tutorial/quickstart/rollup.config.js
@@ -1,16 +1,26 @@
 import babel from '@rollup/plugin-babel'
 import { defineConfig } from 'rollup'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  input: 'src/main.js',
+  input: ['src/main.js'],
   output: {
+    // dir: 'dist',
     file: 'dist/bundle.cjs.js',//输出文件的路径和名称
-    format: 'cjs',//五种输出格式：amd/es6/iife/umd/cjs
-    name: 'bundleName'//当format为iife和umd时必须提供，将作为全局变量挂在window下
+    format: 'iife',//五种输出格式：amd/es6/iife/umd/cjs
+    name: 'bundleName',//当format为iife和umd时必须提供，将作为全局变量挂在window下
+    globals: {
+      lodash: '_', //告诉rollup全局变量_即是lodash
+      jquery: '$' //告诉rollup全局变量$即是jquery
+    }
   },
+  external: ['lodash', 'jquery'],
   plugins: [
     babel({
       exclude: "node_modules/**"
-    })
+    }),
+    resolve(),
+    commonjs()
   ]
 })
--- tutorial/quickstart/src/main.js
+++ tutorial/quickstart/src/main.js
@@ -1,1 +1,5 @@
-import './syntax'
+import _ from 'lodash';
+import $ from 'jquery';
+console.log(_.concat([1, 2, 3], 4, 5));
+console.log($);
+export default 'main';

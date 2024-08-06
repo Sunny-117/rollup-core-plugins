58051e17999e44c28be22738fbf53b3d66fe0f36
Sunny-117 <zhiqiangfu6@gmail.com>
2024-08-06 15:25:21 +0800

    feat: init plugins

---
+++ tutorial/plugins/debugger.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import { rollup, watch } from 'rollup'
+import inputOptions from './rollup.config.js'
+async function run() {
+  //打包阶段
+  const bundle = await rollup(inputOptions)
+  //生成阶段
+  await bundle.generate(inputOptions.output)
+  //写入阶段
+  await bundle.write(inputOptions.output)
+
+  // const watcher = watch(inputOptions);
+  // watcher.on('event', event => {
+  //   console.log(event);
+  // });
+  // setTimeout(() => {
+  //   watcher.close();
+  // }, 1000);
+
+  //关闭阶段
+  await bundle.close()
+}
+
+await run()
